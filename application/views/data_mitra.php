4788f0e14e12c5e9846e9a6e58b3f1c711972452
fahmi <fauzifahmi55@gmail.com>
2022-08-19 22:59:17 +0700

    typo class

--- application/views/data_mitra.php
+++ application/views/data_mitra.php
@@ -77,10 +77,10 @@
    </div>
 </div>
 <div class="modal fade" id="modalImages" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-   <div class="modal-dialog modal-dialog-centered">
+   <div class="modal-dialog-lg modal-dialog-centered">
       <div class="modal-content">
          <div class="modal-body">
-            <img src="" class="image-modal">
+            <img src="" width="100%" class="image-modal">
          </div>
          <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
