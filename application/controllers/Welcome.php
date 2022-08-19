e79c5bee75f701fcaad2dc7711285e06268ba274
fahmi <fauzifahmi55@gmail.com>
2022-08-19 09:35:24 +0700

    typo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,11 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+	
+	function __construct() {
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{
 		$data['title'] = 'Halaman Tambah Gerai - Teh Manis Jumbo';
